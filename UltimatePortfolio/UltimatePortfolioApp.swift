f0562cd7a20376b2f6edb35e11d111fd4c2ba674
AndreAlmeida-89 <andre_rdo@hotmail.com>
2021-06-06 10:48:52 -0300

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Andre Almeida on 06/06/21.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
